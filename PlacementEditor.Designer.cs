e32367007df01f7a266fd4ac37d258373b346522
PenneyMarie1901 <49008947+PenneyMarie1901@users.noreply.github.com>
2020-12-20 10:35:29 -0800

    update code project files to version #2.0 files

--- PlacementEditor.Designer.cs
+++ PlacementEditor.Designer.cs
@@ -42,6 +42,22 @@
             this.label3 = new System.Windows.Forms.Label();
             this.SaveModdedPositions = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblRotation = new System.Windows.Forms.Label();
+            this.lblRotZ = new System.Windows.Forms.Label();
+            this.lblRotY = new System.Windows.Forms.Label();
+            this.lblRotX = new System.Windows.Forms.Label();
+            this.txtRotZ = new System.Windows.Forms.TextBox();
+            this.txtRotY = new System.Windows.Forms.TextBox();
+            this.txtRotX = new System.Windows.Forms.TextBox();
+            this.lblScale = new System.Windows.Forms.Label();
+            this.lblScaleZ = new System.Windows.Forms.Label();
+            this.lblScaleY = new System.Windows.Forms.Label();
+            this.lblScaleX = new System.Windows.Forms.Label();
+            this.txtScaleZ = new System.Windows.Forms.TextBox();
+            this.txtScaleY = new System.Windows.Forms.TextBox();
+            this.txtScaleX = new System.Windows.Forms.TextBox();
+            this.btnApplyScale = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -51,7 +67,7 @@
             this.fileToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(505, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(987, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -67,14 +83,14 @@
             // openSIMPPLATToolStripMenuItem
             // 
             this.openSIMPPLATToolStripMenuItem.Name = "openSIMPPLATToolStripMenuItem";
-            this.openSIMPPLATToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openSIMPPLATToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.openSIMPPLATToolStripMenuItem.Text = "Open SIMP/PLAT...";
             this.openSIMPPLATToolStripMenuItem.Click += new System.EventHandler(this.OpenSIMPPLATToolStripMenuItem_Click);
             // 
             // exitTheSoftwareToolStripMenuItem
             // 
             this.exitTheSoftwareToolStripMenuItem.Name = "exitTheSoftwareToolStripMenuItem";
-            this.exitTheSoftwareToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitTheSoftwareToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
             this.exitTheSoftwareToolStripMenuItem.Text = "Exit The Software";
             this.exitTheSoftwareToolStripMenuItem.Click += new System.EventHandler(this.exitTheSoftwareToolStripMenuItem_Click);
             // 
@@ -85,29 +101,29 @@
             // 
             // txtPosX
             // 
-            this.txtPosX.Location = new System.Drawing.Point(12, 55);
+            this.txtPosX.Location = new System.Drawing.Point(67, 143);
             this.txtPosX.Name = "txtPosX";
-            this.txtPosX.Size = new System.Drawing.Size(481, 20);
+            this.txtPosX.Size = new System.Drawing.Size(215, 20);
             this.txtPosX.TabIndex = 2;
             // 
             // txtPosY
             // 
-            this.txtPosY.Location = new System.Drawing.Point(12, 101);
+            this.txtPosY.Location = new System.Drawing.Point(67, 189);
             this.txtPosY.Name = "txtPosY";
-            this.txtPosY.Size = new System.Drawing.Size(481, 20);
+            this.txtPosY.Size = new System.Drawing.Size(215, 20);
             this.txtPosY.TabIndex = 3;
             // 
             // txtPosZ
             // 
-            this.txtPosZ.Location = new System.Drawing.Point(12, 147);
+            this.txtPosZ.Location = new System.Drawing.Point(67, 235);
             this.txtPosZ.Name = "txtPosZ";
-            this.txtPosZ.Size = new System.Drawing.Size(480, 20);
+            this.txtPosZ.Size = new System.Drawing.Size(214, 20);
             this.txtPosZ.TabIndex = 4;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(218, 39);
+            this.label1.Location = new System.Drawing.Point(7, 146);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(54, 13);
             this.label1.TabIndex = 5;
@@ -116,7 +132,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(218, 85);
+            this.label2.Location = new System.Drawing.Point(7, 192);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(54, 13);
             this.label2.TabIndex = 6;
@@ -125,7 +141,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(218, 131);
+            this.label3.Location = new System.Drawing.Point(7, 238);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 7;
@@ -133,11 +149,11 @@
             // 
             // SaveModdedPositions
             // 
-            this.SaveModdedPositions.Location = new System.Drawing.Point(12, 187);
+            this.SaveModdedPositions.Location = new System.Drawing.Point(12, 372);
             this.SaveModdedPositions.Name = "SaveModdedPositions";
-            this.SaveModdedPositions.Size = new System.Drawing.Size(480, 88);
+            this.SaveModdedPositions.Size = new System.Drawing.Size(963, 30);
             this.SaveModdedPositions.TabIndex = 8;
-            this.SaveModdedPositions.Text = "Save Modified Positions";
+            this.SaveModdedPositions.Text = "Save Modified Data";
             this.SaveModdedPositions.UseVisualStyleBackColor = true;
             this.SaveModdedPositions.Click += new System.EventHandler(this.SaveModdedPositions_Click);
             // 
@@ -145,11 +161,160 @@
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(149, 99);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Position";
+            // 
+            // lblRotation
+            // 
+            this.lblRotation.AutoSize = true;
+            this.lblRotation.Location = new System.Drawing.Point(486, 102);
+            this.lblRotation.Name = "lblRotation";
+            this.lblRotation.Size = new System.Drawing.Size(47, 13);
+            this.lblRotation.TabIndex = 16;
+            this.lblRotation.Text = "Rotation";
+            // 
+            // lblRotZ
+            // 
+            this.lblRotZ.AutoSize = true;
+            this.lblRotZ.Location = new System.Drawing.Point(344, 241);
+            this.lblRotZ.Name = "lblRotZ";
+            this.lblRotZ.Size = new System.Drawing.Size(57, 13);
+            this.lblRotZ.TabIndex = 15;
+            this.lblRotZ.Text = "Rotation Z";
+            // 
+            // lblRotY
+            // 
+            this.lblRotY.AutoSize = true;
+            this.lblRotY.Location = new System.Drawing.Point(344, 195);
+            this.lblRotY.Name = "lblRotY";
+            this.lblRotY.Size = new System.Drawing.Size(57, 13);
+            this.lblRotY.TabIndex = 14;
+            this.lblRotY.Text = "Rotation Y";
+            // 
+            // lblRotX
+            // 
+            this.lblRotX.AutoSize = true;
+            this.lblRotX.Location = new System.Drawing.Point(344, 149);
+            this.lblRotX.Name = "lblRotX";
+            this.lblRotX.Size = new System.Drawing.Size(57, 13);
+            this.lblRotX.TabIndex = 13;
+            this.lblRotX.Text = "Rotation X";
+            // 
+            // txtRotZ
+            // 
+            this.txtRotZ.Location = new System.Drawing.Point(404, 238);
+            this.txtRotZ.Name = "txtRotZ";
+            this.txtRotZ.Size = new System.Drawing.Size(214, 20);
+            this.txtRotZ.TabIndex = 12;
+            // 
+            // txtRotY
+            // 
+            this.txtRotY.Location = new System.Drawing.Point(404, 192);
+            this.txtRotY.Name = "txtRotY";
+            this.txtRotY.Size = new System.Drawing.Size(215, 20);
+            this.txtRotY.TabIndex = 11;
+            // 
+            // txtRotX
+            // 
+            this.txtRotX.Location = new System.Drawing.Point(404, 146);
+            this.txtRotX.Name = "txtRotX";
+            this.txtRotX.Size = new System.Drawing.Size(215, 20);
+            this.txtRotX.TabIndex = 10;
+            // 
+            // lblScale
+            // 
+            this.lblScale.AutoSize = true;
+            this.lblScale.Location = new System.Drawing.Point(823, 102);
+            this.lblScale.Name = "lblScale";
+            this.lblScale.Size = new System.Drawing.Size(34, 13);
+            this.lblScale.TabIndex = 23;
+            this.lblScale.Text = "Scale";
+            // 
+            // lblScaleZ
+            // 
+            this.lblScaleZ.AutoSize = true;
+            this.lblScaleZ.Location = new System.Drawing.Point(671, 241);
+            this.lblScaleZ.Name = "lblScaleZ";
+            this.lblScaleZ.Size = new System.Drawing.Size(44, 13);
+            this.lblScaleZ.TabIndex = 22;
+            this.lblScaleZ.Text = "Scale Z";
+            // 
+            // lblScaleY
+            // 
+            this.lblScaleY.AutoSize = true;
+            this.lblScaleY.Location = new System.Drawing.Point(671, 195);
+            this.lblScaleY.Name = "lblScaleY";
+            this.lblScaleY.Size = new System.Drawing.Size(44, 13);
+            this.lblScaleY.TabIndex = 21;
+            this.lblScaleY.Text = "Scale Y";
+            // 
+            // lblScaleX
+            // 
+            this.lblScaleX.AutoSize = true;
+            this.lblScaleX.Location = new System.Drawing.Point(671, 149);
+            this.lblScaleX.Name = "lblScaleX";
+            this.lblScaleX.Size = new System.Drawing.Size(44, 13);
+            this.lblScaleX.TabIndex = 20;
+            this.lblScaleX.Text = "Scale X";
+            // 
+            // txtScaleZ
+            // 
+            this.txtScaleZ.Location = new System.Drawing.Point(731, 238);
+            this.txtScaleZ.Name = "txtScaleZ";
+            this.txtScaleZ.Size = new System.Drawing.Size(214, 20);
+            this.txtScaleZ.TabIndex = 19;
+            // 
+            // txtScaleY
+            // 
+            this.txtScaleY.Location = new System.Drawing.Point(731, 192);
+            this.txtScaleY.Name = "txtScaleY";
+            this.txtScaleY.Size = new System.Drawing.Size(215, 20);
+            this.txtScaleY.TabIndex = 18;
+            // 
+            // txtScaleX
+            // 
+            this.txtScaleX.Location = new System.Drawing.Point(731, 146);
+            this.txtScaleX.Name = "txtScaleX";
+            this.txtScaleX.Size = new System.Drawing.Size(215, 20);
+            this.txtScaleX.TabIndex = 17;
+            // 
+            // btnApplyScale
+            // 
+            this.btnApplyScale.Location = new System.Drawing.Point(779, 264);
+            this.btnApplyScale.Name = "btnApplyScale";
+            this.btnApplyScale.Size = new System.Drawing.Size(116, 23);
+            this.btnApplyScale.TabIndex = 24;
+            this.btnApplyScale.Text = "Apply X to all fields";
+            this.btnApplyScale.UseVisualStyleBackColor = true;
+            this.btnApplyScale.Click += new System.EventHandler(this.button1_Click);
+            // 
             // MainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(505, 287);
+            this.ClientSize = new System.Drawing.Size(987, 414);
+            this.Controls.Add(this.btnApplyScale);
+            this.Controls.Add(this.lblScale);
+            this.Controls.Add(this.lblScaleZ);
+            this.Controls.Add(this.lblScaleY);
+            this.Controls.Add(this.lblScaleX);
+            this.Controls.Add(this.txtScaleZ);
+            this.Controls.Add(this.txtScaleY);
+            this.Controls.Add(this.txtScaleX);
+            this.Controls.Add(this.lblRotation);
+            this.Controls.Add(this.lblRotZ);
+            this.Controls.Add(this.lblRotY);
+            this.Controls.Add(this.lblRotX);
+            this.Controls.Add(this.txtRotZ);
+            this.Controls.Add(this.txtRotY);
+            this.Controls.Add(this.txtRotX);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.SaveModdedPositions);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
@@ -161,6 +326,7 @@
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainWindow";
             this.Text = "Placement Editor";
+            this.Load += new System.EventHandler(this.MainWindow_Load);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -183,6 +349,22 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button SaveModdedPositions;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblRotation;
+        private System.Windows.Forms.Label lblRotZ;
+        private System.Windows.Forms.Label lblRotY;
+        private System.Windows.Forms.Label lblRotX;
+        private System.Windows.Forms.TextBox txtRotZ;
+        private System.Windows.Forms.TextBox txtRotY;
+        private System.Windows.Forms.TextBox txtRotX;
+        private System.Windows.Forms.Label lblScale;
+        private System.Windows.Forms.Label lblScaleZ;
+        private System.Windows.Forms.Label lblScaleY;
+        private System.Windows.Forms.Label lblScaleX;
+        private System.Windows.Forms.TextBox txtScaleZ;
+        private System.Windows.Forms.TextBox txtScaleY;
+        private System.Windows.Forms.TextBox txtScaleX;
+        private System.Windows.Forms.Button btnApplyScale;
     }
 }
 
--- PlacementEditor.cs
+++ PlacementEditor.cs
@@ -26,6 +26,14 @@ namespace PlacementEditor
         byte[] posX;
         byte[] posY;
         byte[] posZ;
+
+        byte[] rotX;
+        byte[] rotY;
+        byte[] rotZ;
+
+        byte[] scaleX;
+        byte[] scaleY;
+        byte[] scaleZ;
         public void SaveModdedPositions_Click(object sender, EventArgs e)
         {
             SaveFileDialog sfd = new SaveFileDialog();
@@ -43,15 +51,57 @@ namespace PlacementEditor
                 posX = BitConverter.GetBytes(all1);
                 posY = BitConverter.GetBytes(all2);
                 posZ = BitConverter.GetBytes(all3);
+
+                float parseRotX = float.Parse(txtRotX.Text);
+                float parseRotY = float.Parse(txtRotY.Text);
+                float parseRotZ = float.Parse(txtRotZ.Text);
+                float q = parseRotX;
+                float w = parseRotY;
+                float r = parseRotZ;
+                float all4 = float.Parse(q.ToString());
+                float all5 = float.Parse(w.ToString());
+                float all6 = float.Parse(r.ToString());
+                rotX = BitConverter.GetBytes(all4);
+                rotY = BitConverter.GetBytes(all5);
+                rotZ = BitConverter.GetBytes(all6);
+
+                float parseScaleX = float.Parse(txtScaleX.Text);
+                float parseScaleY = float.Parse(txtScaleY.Text);
+                float parseScaleZ = float.Parse(txtScaleZ.Text);
+                float a = parseScaleX;
+                float l = parseScaleY;
+                float d = parseScaleZ;
+                float all7 = float.Parse(a.ToString());
+                float all8 = float.Parse(l.ToString());
+                float all9 = float.Parse(d.ToString());
+                scaleX = BitConverter.GetBytes(all7);
+                scaleY = BitConverter.GetBytes(all8);
+                scaleZ = BitConverter.GetBytes(all9);
                 if (BitConverter.IsLittleEndian)
                 {
                     Array.Reverse(posX);
                     Array.Reverse(posY);
                     Array.Reverse(posZ);
+
+                    Array.Reverse(rotX);
+                    Array.Reverse(rotY);
+                    Array.Reverse(rotZ);
+
+                    Array.Reverse(scaleX);
+                    Array.Reverse(scaleY);
+                    Array.Reverse(scaleZ);
                 }
                 posX.CopyTo(x, 0x2C);
                 posY.CopyTo(x, 0x30);
                 posZ.CopyTo(x, 0x34);
+
+                rotX.CopyTo(x, 0x20);
+                rotY.CopyTo(x, 0x24);
+                rotZ.CopyTo(x, 0x28);
+
+                scaleX.CopyTo(x, 0x38);
+                scaleY.CopyTo(x, 0x3C);
+                scaleZ.CopyTo(x, 0x40);
 
 
 
@@ -72,16 +122,52 @@ namespace PlacementEditor
                     Array.Reverse(x, 0x2C, 4);
                     Array.Reverse(x, 0x30, 4);
                     Array.Reverse(x, 0x34, 4);
+
+                    Array.Reverse(x, 0x20, 4);
+                    Array.Reverse(x, 0x24, 4);
+                    Array.Reverse(x, 0x28, 4);
+
+                    Array.Reverse(x, 0x38, 4);
+                    Array.Reverse(x, 0x3C, 4);
+                    Array.Reverse(x, 0x40, 4);
+
                 }
                 float myFloat = System.BitConverter.ToSingle(PlaceableData, 0x2C); //Read Position X's Float
                 float myFloat2 = System.BitConverter.ToSingle(PlaceableData, 0x30); //Read Position Y's Float
                 float myFloat3 = System.BitConverter.ToSingle(PlaceableData, 0x34); //Read Position Z's Float
+
+                float myFloat4 = System.BitConverter.ToSingle(PlaceableData, 0x20); //Read Roation X's Float
+                float myFloat5 = System.BitConverter.ToSingle(PlaceableData, 0x24); //Read Rotation Y's Float
+                float myFloat6 = System.BitConverter.ToSingle(PlaceableData, 0x28); //Read Rotation Z's Float
+
+                float myFloat7 = System.BitConverter.ToSingle(PlaceableData, 0x38); //Read Scale X's Float
+                float myFloat8 = System.BitConverter.ToSingle(PlaceableData, 0x3C); //Read Scale Y's Float
+                float myFloat9 = System.BitConverter.ToSingle(PlaceableData, 0x40); //ReadScale Z's Float
                 string z = myFloat.ToString();
                 string j = myFloat2.ToString();
                 string i = myFloat3.ToString();
+
+                string q = myFloat4.ToString();
+                string w = myFloat5.ToString();
+                string r = myFloat6.ToString();
+
+                string t = myFloat7.ToString();
+                string y = myFloat8.ToString();
+                string o = myFloat9.ToString();
+
                 txtPosX.Text = z;
                 txtPosY.Text = j;
                 txtPosZ.Text = i;
+
+                txtRotX.Text = q;
+                txtRotY.Text = w;
+                txtRotZ.Text = r;
+
+                txtScaleX.Text = t;
+                txtScaleY.Text = y;
+                txtScaleZ.Text = o;
+
+
                 ofd.Dispose();
                 
             }
@@ -90,6 +176,20 @@ namespace PlacementEditor
         private void exitTheSoftwareToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void MainWindow_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string input1 = txtScaleX.Text;
+
+            txtScaleY.Text = input1;
+            txtScaleZ.Text = input1;
         }
     }
 }
